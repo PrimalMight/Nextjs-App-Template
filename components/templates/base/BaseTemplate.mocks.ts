cbae9bd28e47c3ef6efe431ccfb1ae3debdc486c
PrimalMight <xbuzov00@vutbr.cz>
2022-09-15 23:51:20 +0200

    feat: add BaseTemplate component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,11 @@
+// for testing purposes mocking props to quickly test and develop components
+
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Hello world!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
